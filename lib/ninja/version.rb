12d72b95a2dcb1c2d1883043db85811bac5c8e40
Michael Williams <m.t.williams@live.com>
2016-03-08 21:47:04 -0800

    Woops. Ninja::Build expects inputs to be an array.

--- lib/ninja/version.rb
+++ lib/ninja/version.rb
@@ -1,6 +1,6 @@
 module Ninja
   module VERSION #:nodoc:
-    MAJOR, MINOR, PATCH, PRE = [0, 1, 2, 0]
+    MAJOR, MINOR, PATCH, PRE = [0, 1, 2, 1]
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
 
